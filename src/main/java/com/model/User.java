c87b2b849cbfd277d350fba466d884f30a1f5b23
Jorge Harbes <jharbes@hotmail.com>
2023-02-11 13:16:08 -0300

    acerto de relacionamento da entidade User com a entidade Brand ManyToOne para ManyToMany

--- src/main/java/com/model/User.java
+++ src/main/java/com/model/User.java
@@ -1,5 +1,6 @@
 package com.model;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
@@ -16,76 +19,97 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity(name = "_user")
 public class User implements UserDetails {
-	@Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String username;
 	private String password;
-	@ManyToOne
-	private Brand brand; 
+
+	@ManyToMany
+	@JoinTable(name = "user_brand", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "brand_id"))
+	private List<Brand> brands = new ArrayList<>();
+
 	@ManyToOne
 	private Role role;
 	@Column
 	private String img;
-	
+
 	public Role getRole() {
 		return role;
 	}
+
 	public void setRole(Role role) {
 		this.role = role;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	public Brand getBrand() {
-		return brand;
+
+	public List<Brand> getBrands() {
+		return brands;
 	}
-	public void setBrand(Brand brand) {
-		this.brand = brand;
+
+	public void setBrands(List<Brand> brands) {
+		this.brands = brands;
 	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return true;
 	}
+
 	public String getImg() {
 		return img;
 	}
+
 	public void setImg(String img) {
 		this.img = img;
 	}
