b0c2dee4bd67ea920cb12cc8932298b8d9b2e1f5
Jorge Harbes <jharbes@hotmail.com>
2023-02-11 12:59:48 -0300

    atualizacao DataFileController de um idBrand para lista de idBrands

--- src/main/java/com/controller/DataFileController.java
+++ src/main/java/com/controller/DataFileController.java
@@ -1,40 +1,22 @@
 package com.controller;
 
 import java.math.BigInteger;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.controller.dto.DataFileDTO;
 import com.controller.dto.DataFileOnlyDetailsDTO;
 import com.controller.dto.DataFileOnlyPhotoDTO;
-import com.controller.dto.filter.FilterDTO;
-import com.controller.dto.filter.FilterGalleryDTO;
 import com.form.FilterForm;
-import com.model.DataFile;
-import com.model.DetailProduct;
-import com.model.Project;
 import com.repository.DataFileRepository;
 import com.repository.DataFileRepositoryImp;
 import com.service.BrandService;
@@ -43,7 +25,6 @@ import com.service.DetailProductService;
 import com.service.PhotoService;
 import com.service.PromoterService;
 import com.service.ShopService;
-import com.turkraft.springfilter.boot.Filter;
 import com.util.LocalDateConverter;
 
 @RestController
@@ -69,60 +50,70 @@ public class DataFileController {
 
 	@ResponseBody
 	@PostMapping("/photos")
-	public ResponseEntity listPhotos(@RequestParam(name = "initialDate",required = false) String initialDate,@RequestParam  String finalDate
-			,@RequestParam long idBrand, @RequestBody  FilterForm filter) {
+	public ResponseEntity listPhotos(@RequestParam(name = "initialDate", required = false) String initialDate,
+			@RequestParam String finalDate, @RequestParam List<Long> idBrandList, @RequestBody FilterForm filter) {
 		try {
 			List<DataFileOnlyPhotoDTO> dtos = new ArrayList<>();
-			List<Object> datas;
-			if(filter!=null) {
-				 datas = dataFileService.getPhotos(LocalDateConverter.convertToLocalDate(initialDate) , LocalDateConverter.convertToLocalDate(finalDate)
-						,idBrand,filter.getFilter());
-			}else {
-				 datas = dataFileService.getPhotos(LocalDateConverter.convertToLocalDate(initialDate) , LocalDateConverter.convertToLocalDate(finalDate)
-						,idBrand,null);
+			List<Object> datas = new ArrayList<>();
+			if (filter != null) {
+				for (Long idBrand: idBrandList) {
+				datas.add(dataFileService.getPhotos(LocalDateConverter.convertToLocalDate(initialDate),
+						LocalDateConverter.convertToLocalDate(finalDate), idBrand, filter.getFilter()));
+				}
+			} else {
+				// realmente necessario caso filter == null?
+				for (Long idBrand: idBrandList) {
+				datas.add(dataFileService.getPhotos(LocalDateConverter.convertToLocalDate(initialDate),
+						LocalDateConverter.convertToLocalDate(finalDate), idBrand, null));
+				}
 			}
-			
-			for(Object obj: datas) {
+
+			for (Object obj : datas) {
 				Object[] cast = (Object[]) obj;
 				DataFileOnlyPhotoDTO dto = new DataFileOnlyPhotoDTO();
 				dto.setId(((BigInteger) cast[0]).longValue());
-				dto.setBrand(brandService.convertToDto(brandService.getBrandById(((BigInteger) cast[1]).longValue()))); 
-				dto.setShop(shopService.convertToDto(shopService.getBrandById(((BigInteger) cast[2]).longValue()))); 
+				dto.setBrand(brandService.convertToDto(brandService.getBrandById(((BigInteger) cast[1]).longValue())));
+				dto.setShop(shopService.convertToDto(shopService.getBrandById(((BigInteger) cast[2]).longValue())));
 				dto.setDate(((java.sql.Date) cast[3]).toLocalDate());
-				dto.setPromoter(promoterService.convertToDto(promoterService.getBrandById(((BigInteger)cast[5]).longValue())));
-                dto.setPhotos(photoService.convertToDTOS(photoService.getPhotosByDataFile(dto.getId())));
+				dto.setPromoter(
+						promoterService.convertToDto(promoterService.getBrandById(((BigInteger) cast[5]).longValue())));
+				dto.setPhotos(photoService.convertToDTOS(photoService.getPhotosByDataFile(dto.getId())));
 				dtos.add(dto);
 			}
 			return ResponseEntity.status(HttpStatus.OK).body(dtos);
 		} catch (Exception e) {
-			return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
-	
+
 	@ResponseBody
 	@PostMapping("/details")
-	public ResponseEntity listDetails(@RequestParam String initialDate,@RequestParam  String finalDate
-			,@RequestParam long idBrand, @RequestBody  FilterForm filter) {
+	public ResponseEntity listDetails(@RequestParam String initialDate, @RequestParam String finalDate,
+			@RequestParam List<Long> idBrandList, @RequestBody FilterForm filter) {
 		try {
 			List<DataFileOnlyDetailsDTO> dtos = new ArrayList<>();
-			List<Object> datas = dataFileService.getDetails(LocalDateConverter.convertToLocalDate(initialDate) , LocalDateConverter.convertToLocalDate(finalDate)
-					,idBrand,filter.getFilter());
-			for(Object obj: datas) {
+			List<Object> datas = new ArrayList<>();
+				for (Long idBrand: idBrandList) {
+					datas.add(dataFileService.getDetails(LocalDateConverter.convertToLocalDate(initialDate),
+					LocalDateConverter.convertToLocalDate(finalDate), idBrand, filter.getFilter()));
+				}
+			for (Object obj : datas) {
 				Object[] cast = (Object[]) obj;
 				DataFileOnlyDetailsDTO dto = new DataFileOnlyDetailsDTO();
 				dto.setId(((BigInteger) cast[0]).longValue());
-				dto.setBrand(brandService.convertToDto(brandService.getBrandById(((BigInteger) cast[1]).longValue()))); 
-				dto.setShop(shopService.convertToDto(shopService.getBrandById(((BigInteger) cast[2]).longValue()))); 
+				dto.setBrand(brandService.convertToDto(brandService.getBrandById(((BigInteger) cast[1]).longValue())));
+				dto.setShop(shopService.convertToDto(shopService.getBrandById(((BigInteger) cast[2]).longValue())));
 				dto.setDate(((java.sql.Date) cast[3]).toLocalDate());
-				dto.setPromoter(promoterService.convertToDto(promoterService.getBrandById(((BigInteger)cast[5]).longValue())));
-				dto.setDetails(detailProductService.convertToDTOS(detailProductService.getDetailProductByDataFile(dto.getId())));
+				dto.setPromoter(
+						promoterService.convertToDto(promoterService.getBrandById(((BigInteger) cast[5]).longValue())));
+				dto.setDetails(detailProductService
+						.convertToDTOS(detailProductService.getDetailProductByDataFile(dto.getId())));
 				dtos.add(dto);
 			}
 			return ResponseEntity.status(HttpStatus.OK).body(dtos);
 		} catch (Exception e) {
- 			return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
-		}	
+			return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
 	}
-	
-	
+
 }
